83e14606ca07ac32b162381a96964dbdf7a65a56
Mac Mbizo <macmbizo@gmail.com>
2021-11-30 23:33:43 +0200

    Split training data and testing data.

--- main.py
+++ main.py
@@ -14,7 +14,16 @@ def predict_salary(years_of_experience):
     }
 
     df = pd.DataFrame(raw_data)
-    print(df)
+
+    # Get the data we want to predict
+    X = np.array(df['years_worked']).reshape(-1,1)
+    y = np.array(df['salary']).reshape(-1,1)
+
+    # Splits the testing data and the training data
+    train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=0, test_size=.20)
 
 
-predict_salary(12)
+
+predict_salary(10)
+
+
